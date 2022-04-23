105eaf8f6bb0810d1eb398292c2fb5f083d79d25
Sai Kumar <kumarvukkum@gmail.com>
2022-04-23 10:33:00 +0530

    55 feat done

--- src/main/java/com/nt/Spring/App.java
+++ src/main/java/com/nt/Spring/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World! 555" );
+        System.out.println( "Hello World! 55" );
 
         System.out.println( "Hello World! hiii sony liv" );
 
